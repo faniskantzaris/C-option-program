5bed52b1673224933952458cf4c290729501862f
Null <80452309+faniskantzaris@users.noreply.github.com>
2023-12-16 01:20:23 +0200

    Update C_Option_Program.c

--- C_Option_Program.c
+++ C_Option_Program.c
@@ -1,21 +1,18 @@
-//ONOMATEPONIMO: KANTZARIS THEOFANIS
-//AM: 1117-2020-00062
 #include <stdio.h>
 #include <string.h>
 
-void replacechar(char *word,char c1,char c2); //arxikopoihsh sinartisis gia antikatastasi
-
-int checkpalindrome(char *word); //arxikopoihsh sinartisis gia na doume an einai karkiniki epigrafi
+void replacechar(char *word,char c1,char c2); 
+int checkpalindrome(char *word); 
 
 int main()
 {
     char a, word[1000], c1, c2;
-    int i, freq = 0,flag = 0, num, ch; //me to freq briskoume thn sixnotita
+    int i, freq = 0,flag = 0, num, ch; 
 
-    printf("Dwse ena string: "); //o xristis dinei thn epilogh tou
+    printf("Dwse ena string: "); 
     scanf("%s", &word);
 
-    if (strlen(word) > 50) // se periptosh pou to string pou dinei einai parapano apo 50
+    if (strlen(word) > 50) 
     {
     	printf("\nTo string einai megalytero apo 50 xaraktires\n");
 	return 0;
@@ -36,39 +33,39 @@ int main()
 
             scanf(" %c", &a);
             printf("H thesi tou grammatos %c sthn leji %s einai:\n", a,word);
-            for (i = 0; i < strlen(word); i++) //briskoume thn thesi tou xaraktira
+            for (i = 0; i < strlen(word); i++) 
             {
                 if (word[i] == a)
                 {
-                    flag = 1; //briskoume tin thesi tou xaktira
+                    flag = 1; 
                     printf("%d\n", i + 1);
-                    freq++; //briskoume tin sixnotita
+                    freq++; 
                 }
             }
-            if (flag) //an to flag einai 1
+            if (flag) 
             {
-                printf("O xaraktiras  %c brethike %d fores.\n", a, freq); //mas deixnei tn thesi kai tin sixnotita
+                printf("O xaraktiras  %c brethike %d fores.\n", a, freq); 
             }
             else
             {
-                printf("Kanena apotelesma\n"); //se periptoi lathos xaraktira
+                printf("Kanena apotelesma\n"); 
             }
-	    freq = 0; //arkikopoihsh tou freq oste ama jana dothei h epilogh na einai 0
+	    freq = 0;
         }
         else if (num == 2)
         {
             printf("Dwse ton xaraktira poy thes na antikatastisis: ");
-            scanf(" %c", &c1); //eisagogi toy xaraktira
+            scanf(" %c", &c1);
             printf("Dwse ton xaraktira na antikatastathi me %c: ",c1);
-            scanf(" %c", &c2); //eisagogi tou xaraktira antikatastasis
+            scanf(" %c", &c2); 
             printf("Prin thn antikatastasi:%s\n",word);
             printf("meta thn antikatastasi:");
-            replacechar(word, c1, c2); //kaloume tin sinartisi antikatastasis
+            replacechar(word, c1, c2); 
 	    printf("\n");
 	}
         else if (num == 3)
         {
-            if (checkpalindrome(word)) //kaloume thn sinartisi gia karkiniki epigrafi
+            if (checkpalindrome(word)) 
                 printf("To string einai karkiniki epigrafi\n");
             else
                 printf("To string den einai karkiniki epigrafi\n");
@@ -79,36 +76,36 @@ int main()
 	}
 	else
 	{
-		printf("Mi diathesimi epilogh\n"); //se periptosi lathos epilogis
-		while ( getchar() != '\n' ); //adeiasma eisagogis
+		printf("Mi diathesimi epilogh\n");
+		while ( getchar() != '\n' );
 	}
-    } while (1); //termatizei me thn epilogh 4
+    } while (1);
 
     return 0;
 }
 
 
-void replacechar(char *word, char c1,char c2) //h sinartisi gia thn antikatastasi
+void replacechar(char *word, char c1,char c2) 
 {
     int i = 0;
-    for (i = 0; i<strlen(word); i++) //tsekaroume thn epilogi pou eisagete
+    for (i = 0; i<strlen(word); i++) 
     {
-        if (word[i] == c1) //an einai idios o xaraktiras
+        if (word[i] == c1) 
         {
-	    printf("%c", c2); //ton antikathista
+	    printf("%c", c2); 
         }
 	else
 	{
-	    printf("%c", word[i]); //alliws ektiponei pali ton arxiko
+	    printf("%c", word[i]);
 	}
     }
 }
 
-int checkpalindrome(char *word) //h sinartisi gia na doume an einai karkiniki epigrafo
+int checkpalindrome(char *word) 
 {
-    int i, c = 0,n; //arxikopoihsh tou c
+    int i, c = 0,n;
     n = strlen(word);
-    for (i = 0; i < n/2; i++) //elegxos karkinikis epigrafis
+    for (i = 0; i < n/2; i++)
     {
         if (word[i] == word[n-i-1])
         	c++;
